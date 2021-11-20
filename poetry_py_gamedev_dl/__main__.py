2ccfe3e869ad4fd548d48f8f8fd67039727d4a4a
jm8 <joshua.sims@hilliardschools.org>
2021-11-20 17:07:14 -0500

    Everything works but krita

--- poetry_py_gamedev_dl/__main__.py
+++ poetry_py_gamedev_dl/__main__.py
@@ -1,4 +1,3 @@
-import requests
 from zipfile import ZipFile
 from pathlib import Path
 import subprocess
@@ -7,6 +6,9 @@ from bs4 import BeautifulSoup
 import win32com.client
 import sys
 from gooey import Gooey, GooeyParser
+import asyncio
+import aiofiles
+import aiohttp
 
 if getattr(sys, 'frozen', False):
     datapath = Path(sys._MEIPASS)
@@ -15,15 +17,14 @@ else:
     selfpath = Path(__file__)
     datapath = selfpath.parent
     delete_path = None
-
 log = print
 
 
-def get_release_url(repo, suffix):
+async def get_release_url(session, repo, suffix):
     url = f'https://api.github.com/repos/{repo}/releases/latest'
     log(f'Looking at {url}...')
-    r = requests.get(url, headers={'Accept': 'application/vnd.github.v3+json'})
-    for asset in r.json()['assets']:
+    r = await session.get(url, headers={'Accept': 'application/vnd.github.v3+json'})
+    for asset in (await r.json())['assets']:
         if asset['name'].endswith(suffix):
             log(f'Found asset {asset["name"]}, ending with {suffix}')
             return asset['url']
@@ -37,80 +38,75 @@ def makeshortcut(shortcut_path, target):
     shortcut.save()
 
 
-def log_progress(progress):
-    log(f'Progress: {int(progress*100)}%')
-
-
-def download(url, path, headers={}):
-    if path.exists():
-        log(f'Already have {path}')
-        return
-    response = requests.get(url, stream=True, headers=headers)
-    size = int(response.headers.get('content-length', 0))
-    block_size = 1024
+async def download(session, url, path, headers={}):
     log(f'Downloading {url} to {path}')
-    progress = 0
-    numblocks = 0
-    log_progress(0)
-    with open(path, 'wb') as f:
-        for data in response.iter_content(block_size):
-            f.write(data)
-            numblocks += 1
-            if numblocks % 5000 == 0:
-                log_progress(numblocks*block_size / size)
-    log_progress(1)
+    async with session.get(url, headers=headers) as response:
+        async with aiofiles.open(path, 'wb') as f:
+            await f.write(await response.read())
+            # async for chunk in response.content.iter_chunked(1024*1024):
+            #     await f.write(chunk)
 
 
-def extract_zip(zippath, folder_path):
+async def gettext(session, url, headers={}):
+    log(f'Reading {url}')
+    async with session.get(url, headers=headers) as response:
+        return await response.text()
+
+
+async def extract_zip(zippath, folder_path):
     log(f'Extracting {zippath} to {folder_path}...')
     with ZipFile(zippath) as z:
         infolist = z.infolist()
-        progress = 0
-        log_progress(0)
-        for i, member in enumerate(infolist):
+        for member in infolist:
             z.extract(member, folder_path)
-            if i % 100 == 0:
-                log_progress(i / len(infolist))
-        log_progress(1)
-
-
-heading_printed = False
+            await asyncio.sleep(0)
 
 
 def heading(name, url):
-    global heading_printed
-    if heading_printed:
-        log()
-    heading_printed = True
-    log(name)
-    log(url)
+    log(f'Starting {name}')
 
 
-def download_godot(dlpath, shortcut_folder):
+def finished(name):
+    log(f'Finished {name}')
+
+
+async def download_godot(session, dlpath, shortcut_folder):
     heading('Godot', 'https://godotengine.org/download/windows')
     shortcut_path = Path(shortcut_folder, 'Godot.lnk')
     if shortcut_path.exists():
         log(f'Already have {shortcut_path}')
         return
     log("Finding latest Godot version...")
-    url = get_release_url('godotengine/godot', 'stable_win64.exe.zip')
+    url = await get_release_url(session, 'godotengine/godot', 'stable_win64.exe.zip')
     zippath = Path(dlpath, "godot.exe.zip")
 
-    download(url, zippath, headers={'Accept': 'application/octet-stream'})
+    await download(session, url, zippath, headers={'Accept': 'application/octet-stream'})
 
     log(f'Extracting {zippath}')
     godot_exe = None
-    with ZipFile(zippath) as z:
-        for member in z.infolist():
-            godot_exe = member.filename
-            z.extract(member, dlpath)
+    await extract_zip(zippath, dlpath)
     log(f'Deleting {zippath}')
     zippath.unlink()
 
+    godot_exe = next(dlpath.glob("Godot*.exe"))
+
     makeshortcut(shortcut_path, Path(dlpath, godot_exe))
+    finished('Godot')
 
 
-def download_github_desktop(dlpath, shortcut_folder):
+async def extract_7z(sevenzip_location, archive, outpath):
+    log(f'Using 7zip at {sevenzip_location}')
+    log(f'Extracting {archive} with 7zip...')
+    proc = await asyncio.create_subprocess_exec(str(sevenzip_location), 'x', '-y', '-aos',
+                                                f'{archive}', f'-o{outpath}', stdout=asyncio.subprocess.
+                                                PIPE)
+
+    await proc.wait()
+    if proc.returncode != 0:
+        raise ValueError(f'7zip failed extracting {sevenzip_location}')
+
+
+async def download_github_desktop(session, dlpath, shortcut_folder, clone_repo):
     heading('Github Desktop', 'https://desktop.github.com/')
     sevenzip_location = Path(datapath, '7z/7z.exe')
     shortcut_path = Path(shortcut_folder, 'GithubDesktop.lnk')
@@ -121,33 +117,29 @@ def download_github_desktop(dlpath, shortcut_folder):
     setup_exe_contents_path = Path(dlpath, 'GithubDestkop')
     url = 'https://central.github.com/deployments/desktop/desktop/latest/win32'
 
-    download(url, setup_exe_path)
+    await download(session, url, setup_exe_path)
 
-    log(f'Using 7zip at {sevenzip_location}')
-    log(f'Extracting {setup_exe_path} with 7zip...')
-    x = subprocess.run([str(sevenzip_location), 'x', '-y', '-aos',
-                        f'{setup_exe_path}', f'-o{setup_exe_contents_path}'], stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
-    if x.returncode != 0:
-        raise ValueError('7zip failed')
+    await extract_7z(sevenzip_location, setup_exe_path, setup_exe_contents_path)
 
     log(f'Deleting {setup_exe_path}')
     setup_exe_path.unlink()
 
     nupkg_path = next(setup_exe_contents_path.glob("*.nupkg"))
 
-    log(f'Extracting {nupkg_path} with 7zip...')
-    x = subprocess.run([str(sevenzip_location), 'x', '-y', '-aos',
-                        f'{nupkg_path}', f'-o{setup_exe_contents_path}'], stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
-    if x.returncode != 0:
-        raise ValueError('7zip failed')
+    await extract_7z(sevenzip_location, nupkg_path, setup_exe_contents_path)
 
     github_desktop_exe_path = Path(
         setup_exe_contents_path, 'lib', 'net45', 'GithubDesktop.exe')
     log(f'Github Desktop at {github_desktop_exe_path}')
     makeshortcut(shortcut_path, github_desktop_exe_path)
 
+    finished('Github Desktop')
 
-def download_blender(dlpath, shortcut_folder):
+    if clone_repo:
+        await clone_samples(clone_repo, dlpath=shortcut_folder, gitdlpath=dlpath)
+
+
+async def download_blender(session, dlpath, shortcut_folder):
     heading('Blender', 'https://www.blender.org/download/')
     shortcut_path = Path(shortcut_folder, 'Blender.lnk')
     folder_path = Path(dlpath, 'blender')
@@ -156,23 +148,21 @@ def download_blender(dlpath, shortcut_folder):
         return
     releases_url = "https://mirror.clarkson.edu/blender/release/"
     log(f'Finding latest Blender version at {releases_url}')
-    releases_soup = BeautifulSoup(requests.get(
-        releases_url).content, features='lxml')
+    releases_soup = BeautifulSoup(await gettext(session, releases_url), features='lxml')
     folder_link = releases_soup.find_all(
         'a', href=re.compile(r'Blender\d+\.\d+/'))[-1]
     release_folder_url = releases_url+folder_link['href']
 
     log(f"Finding Blender download at {release_folder_url}")
-    release_folder_soup = BeautifulSoup(requests.get(
-        release_folder_url).content, features='lxml')
+    release_folder_soup = BeautifulSoup(await gettext(session, release_folder_url), features='lxml')
     download_link = release_folder_soup.find_all(
         'a', href=re.compile(r'.*windows.*64.*\.zip'))[-1]
     download_path = release_folder_url+download_link['href']
 
     zippath = Path(dlpath, 'blender.zip')
-    download(download_path, zippath)
+    await download(session, download_path, zippath)
 
-    extract_zip(zippath, folder_path)
+    await extract_zip(zippath, folder_path)
     with ZipFile(zippath) as z:
         z.extractall(folder_path)
     log(f'Deleting {zippath}')
@@ -180,9 +170,10 @@ def download_blender(dlpath, shortcut_folder):
 
     blender_exe = next(folder_path.glob("*/blender.exe"))
     makeshortcut(shortcut_path, blender_exe)
+    finished('Blender')
 
 
-def download_krita(dlpath, shortcut_folder):
+async def download_krita(session, dlpath, shortcut_folder):
     heading('Krita', 'https://krita.org/en/download/krita-desktop/')
     folder_path = Path(dlpath, 'krita')
     shortcut_path = Path(shortcut_folder, 'Krita.lnk')
@@ -191,40 +182,41 @@ def download_krita(dlpath, shortcut_folder):
         return
     releases_url = "https://download.kde.org/stable/krita/"
     log(f'Finding latest Krita version at {releases_url}')
-    releases_soup = BeautifulSoup(requests.get(
-        releases_url).content, features='lxml')
+    releases_soup = BeautifulSoup(await gettext(session, releases_url), features='lxml')
     folder_link = releases_soup.find_all(
         'a', href=re.compile(r'\d+\.\d+\.\d+/'))[-1]
     release_folder_url = releases_url+folder_link['href']
 
     log(f"Finding Krita download at {release_folder_url}")
-    release_folder_soup = BeautifulSoup(requests.get(
-        release_folder_url).content, features='lxml')
+    release_folder_soup = BeautifulSoup(await gettext(session, release_folder_url), features='lxml')
     download_link = release_folder_soup.find_all(
         'a', href=re.compile(r'krita-x64-.*\.zip'))[-1]
     download_path = release_folder_url+download_link['href']
 
     zippath = Path(dlpath, 'krita.zip')
-    download(download_path, zippath)
+    await download(session, download_path, zippath)
 
     log(f'Extracting {zippath} to {folder_path}...')
-    extract_zip(zippath, folder_path)
+    await extract_zip(zippath, folder_path)
     log(f'Deleting {zippath}')
     zippath.unlink()
 
     krita_exe = next(folder_path.glob("*/bin/krita.exe"))
     makeshortcut(shortcut_path, krita_exe)
+    finished('Krita')
 
 
-def clone_samples(repo, dlpath, gitdlpath):
+async def clone_samples(repo, dlpath, gitdlpath):
+    heading('Samples', 'https://github.com/hdb-gamedev/godot-samples')
     gitpath = Path(
         gitdlpath, 'GithubDestkop/lib/net45/resources/app/git/cmd/git.exe')
     log(f'using {gitpath} to clone {repo}')
-    subprocess.run([str(gitpath.resolve()), 'clone', repo],
-                   cwd=dlpath, stdout=subprocess.PIPE)
+    await asyncio.create_subprocess_exec(str(gitpath.resolve()), 'clone', repo,
+                                         cwd=dlpath, stdout=asyncio.subprocess.PIPE)
+    finished('Samples')
 
 
-@Gooey(progress_regex=r'^Progress: (\d+)%$', program_name='Gamedev Club Installer')
+# @Gooey(program_name='Gamedev Club Installer')
 def main():
     default_download_message = ''
     default_download_folder = selfpath.resolve().parent
@@ -234,8 +226,8 @@ def main():
         default_download_folder = Path(default_download_folder, 'gamedev')
         default_download_message = f'Defaulting download folder to {default_download_folder}, since this directory has other things in it'
     log(default_download_message)
-    default_shortcut_folder = Path(Path.home(), 'Desktop')
-    #default_shortcut_folder = default_download_folder
+    # default_shortcut_folder = Path(Path.home(), 'Desktop')
+    default_shortcut_folder = default_download_folder
     log(f'Shortcuts folder defaulting to {default_shortcut_folder}')
 
     parser = GooeyParser(
@@ -260,15 +252,21 @@ def main():
     arguments.download_folder.mkdir(parents=True, exist_ok=True)
     arguments.shortcut_folder.mkdir(parents=True, exist_ok=True)
 
-    download_godot(arguments.download_folder, arguments.shortcut_folder)
-    download_github_desktop(arguments.download_folder,
-                            arguments.shortcut_folder)
-    download_blender(arguments.download_folder, arguments.shortcut_folder)
-    download_krita(arguments.download_folder, arguments.shortcut_folder)
+    async def run_all():
+        async with aiohttp.ClientSession() as session:
+            await asyncio.gather(
+                download_godot(session, arguments.download_folder,
+                               arguments.shortcut_folder),
+                # download_github_desktop(session, arguments.download_folder,
+                #                         arguments.shortcut_folder, clone_repo=arguments.clone_repo),
+                # download_blender(
+                #     session, arguments.download_folder, arguments.shortcut_folder),
+                # download_krita(session, arguments.download_folder,
+                #                arguments.shortcut_folder)
+            )
 
-    if arguments.clone_repo:
-        clone_samples(arguments.clone_repo,
-                      arguments.shortcut_folder, arguments.download_folder)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_all())
 
     # if delete_path is not None:
     #     log(f"This is when I would schedule {delete_path} for deletion")
